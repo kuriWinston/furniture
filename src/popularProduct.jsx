462b442d6269db0f0adbd85ec687318e0b8f8165
Ivan <ivan@gome-pc.come>
2026-07-23 20:33:34 +0300

    update repositor

--- src/popularProduct.jsx
+++ src/popularProduct.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
+import Sects from './sects'
+import "./popProduct.css"
 
 export default function ScrollMenu() {
   const [sections] = useState([
-    { id: 1, title: "Кухні" },
-    { id: 2, title: "Вітальня" },
-    { id: 3, title: "Спальня" },
-    { id: 4, title: "Офіс" },
-    { id: 5, title: "Дитяча" },
-    { id: 6, title: "Гардероб" },
-    { id: 7, title: "Тераса" },
-    { id: 8, title: "Інтер'єр" },
+    { id: 1, title: "Дитяча", img: "/imgTovar/Detskayakomnata.jpg"},
+    { id: 2, title: "Вітальня", img: "/imgTovar/vitalk.jpg" },
+    { id: 3, title: "Спальня", img: "/imgTovar/spalna.jpg" },
+    { id: 4, title: "Офіс", img: "/imgTovar/offis.jpg"},
+    { id: 5, title: "Сад", img: "/imgTovar/garden.jpg"},
+    { id: 6, title: "Гардероб", img: "/imgTovar/garderop.jpg"},
+    { id: 7, title: "Тераса", img: "/imgTovar/terasa.jpg"},
+    { id: 8, title: "Для бізнесу", img: "/imgTovar/dlyabiz.jpg"},
   ]);
 
   return (
@@ -20,12 +22,7 @@ export default function ScrollMenu() {
 
       <div className="brand-menu__grid">
         {sections.map((section) => (
-          <button key={section.id} className="brand-menu__card" type="button">
-            <div className="brand-menu__thumb" aria-hidden="true">
-              <span className="brand-menu__placeholder">Фото</span>
-            </div>
-            <div className="brand-menu__title">{section.title}</div>
-          </button>
+          <Sects key={section.id} section={section} />
         ))}
       </div>
     </section>
